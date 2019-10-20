db1b7483b9ef3f7de4e50b0895860345325ad0c6
Konrad Kolasa <rengar600@interia.pl>
2019-10-20 19:54:07 +0200

    ListView component & ListView actions bar first approach

--- Client/src/components/SignUpComponents/SignUp.js
+++ Client/src/components/SignUpComponents/SignUp.js
@@ -5,7 +5,7 @@ import '../../styles/SignUpStyles/SignUp.scss';
 //SignUpForm nested here
 const SignUp = () => {
     return ( 
-        <main>
+        <main className="signUp">
                 <h1>Zarejestruj się!</h1>
                 <p>I dołącz do ekipy zoorganizowanych kinomaniaków</p>
                 <SignUpForm/>
