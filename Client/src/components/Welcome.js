0733f39943db77837ed363297286cd5b164af566
Konrad Kolasa <rengar600@interia.pl>
2019-10-23 13:20:25 +0200

    TopList finished

--- Client/src/components/Welcome.js
+++ Client/src/components/Welcome.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../styles/ListViewStyles/ListView.scss';
 import TopActions from './TopSeriesComponents/TopActions';
+import TopList from './TopSeriesComponents/TopList';
 const Welcome = () => {
     return ( 
         <main className="listView">
             <TopActions/>
+            <TopList/>
         </main>
      );
 }
